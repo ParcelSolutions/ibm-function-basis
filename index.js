5bcab263e482f6ec7a96556d8da436f6839aff9e
ParcelSolutions <info@parcelsolutions.eu>
2020-02-16 14:07:52 +0100

    better error return

--- index.js
+++ index.js
@@ -5,19 +5,33 @@ const { to } = require("await-to-js");
 async function main(params) {
   let error;
   let result;
+  try {
+    switch ((params.type || "").toLowerCase()) {
+      case "": {
+        return result;
+      }
 
-  switch ((params.type || "").toLowerCase()) {
-    case "": {
-      return result;
+      default:
+        return {
+          error: {
+            code: `unknown type used : ${params.type}`,
+            message: "see log for more information!",
+            type: params.type,
+            request: params.request
+          }
+        };
     }
+  } catch (e) {
+    console.error(e);
 
-    default:
-      return {
-        error: {
-          code: `unknown type used : ${params.type}`,
-          message: params
-        }
-      };
+    return {
+      error: {
+        code: "error while running function",
+        message: "see log for more information!",
+        type: params.type,
+        request: params.request
+      }
+    };
   }
 }
 
