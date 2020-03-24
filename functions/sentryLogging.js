177b5adcc87d25f1bbd329ca483c66656afae1b1
ParcelSolutions <info@parcelsolutions.eu>
2020-03-24 19:37:33 +0100

    add sentry logging

---
+++ functions/sentryLogging.js
@@ -0,0 +1,13 @@
+const Sentry = require("@sentry/node");
+
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    release: "FUNCTIONNAME@1.0",
+    dsn: "https://e17259a66abc42618cfcc95ca89cc882@sentry.io/2627328"
+  });
+}
+function logError(e) {
+  Sentry.captureException(e);
+}
+
+exports.logError = logError;
