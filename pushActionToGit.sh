cb6e3473be41de3892fa0f32e2cbbeaec2104338
ParcelSolutions <info@parcelsolutions.eu>
2020-03-26 12:24:36 +0100

    change push action

--- pushActionToGit.sh
+++ pushActionToGit.sh
@@ -4,7 +4,7 @@
 entryFile=$1
 actionName=$2
 export ENTRY_FILE=$1
-export NODE_ENV='transmate' && webpack --config ibm-function-basis/webpack.config.js
+export NODE_ENV='transmate' && webpack -r esm --config ibm-function-basis/webpack.config.js
 
 echo "get $entryFile , rename to $actionName push to actionsTransmate.git with user $githubUsername"
 git clone https://$githubUsername:$githubToken@github.com/ParcelSolutions/actionsTransmate.git
