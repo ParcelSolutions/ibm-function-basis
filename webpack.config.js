8acf5e195303fce7ceff6136ff5eb81e3019e242
ParcelSolutions <info@parcelsolutions.eu>
2020-03-26 11:14:02 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins,
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: `bundle${isProduction ? "" : "-local"}.js`
   },
   mode: "production",
   module: {
