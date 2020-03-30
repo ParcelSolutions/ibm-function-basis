5c77b421ad7893a472260175c640559b77607783
ParcelSolutions <info@parcelsolutions.eu>
2020-03-30 14:45:58 +0200

    update naming postgres

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { createdDT, MongoConnection } = require("./database/mongo");
-const PG = require("./database/postgress");
+const PostgresConnection = require("./database/postgress");
 const RedisConnection = require("./database/redis");
 const { setEnv } = require("./functions/setEnv");
 const { logError } = require("./functions/sentryLogging");
@@ -7,8 +7,8 @@ const closeConnections = require("./functions/closeConnections");
 
 exports.createdDT = createdDT;
 exports.MongoConnection = MongoConnection;
-exports.PG = PG;
-exports.PostgresConnection = PG;
+exports.PG = PostgresConnection;
+exports.PostgresConnection = PostgresConnection;
 exports.RedisConnection = RedisConnection;
 exports.logError = logError;
 exports.setEnv = setEnv;
