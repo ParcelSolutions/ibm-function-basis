6970c502426b653e82e0278db5cc81e1c8e59abe
ParcelSolutions <info@parcelsolutions.eu>
2020-03-31 10:12:12 +0200

    fix logging sentry

--- functions/sentryLogging.js
+++ functions/sentryLogging.js
@@ -1,25 +1,33 @@
 const Sentry = require("@sentry/node");
+const debug = require("debug")("sentryLogging");
 
 if (process.env.NODE_ENV !== "development" && !process.env.SENTRY_DNS) {
   console.log("sentry not enabled, set SENTRY_DNS to enable");
 }
 
 function logError(e, type, request) {
-  if (process.env.NODE_ENV !== "development" && process.env.SENTRY_DNS) {
+  if (
+    process.env.NODE_ENV !== "development" &&
+    process.env.SENTRY_DNS &&
+    typeof process.env.SENTRY_DNS === "string"
+  ) {
     Sentry.init({
       release: "serverlessFunction",
       dsn: process.env.SENTRY_DNS
     });
-  }
-  Sentry.configureScope(scope => {
-    scope.setExtra("request", request);
-  });
 
-  Sentry.addBreadcrumb({
-    category: type,
-    level: Sentry.Severity.Info
-  });
-  Sentry.captureException(e);
+    Sentry.configureScope(scope => {
+      scope.setExtra("request", request);
+    });
+
+    Sentry.addBreadcrumb({
+      category: type,
+      level: Sentry.Severity.Info
+    });
+    Sentry.captureException(e);
+  } else {
+    debug(e, type, request);
+  }
   return null;
 }
 
--- functions/setEnv.js
+++ functions/setEnv.js
@@ -1,18 +1,43 @@
 exports.setEnv = params => {
-  process.env.POSTGRESS_URI = params.POSTGRESS_URI || process.env.POSTGRESS_URI;
-  process.env.MONGO_URI = params.MONGO_URI || process.env.MONGO_URI;
-  process.env.MONGO_URI_DEV = params.MONGO_URI_DEV || process.env.MONGO_URI_DEV;
-  process.env.MONGO_URI_TEST =
-    params.MONGO_URI_TEST || process.env.MONGO_URI_TEST;
-  process.env.MONGO_URI_LIVE =
-    params.MONGO_URI_LIVE || process.env.MONGO_URI_LIVE;
-  process.env.HERE_appId = params.HERE_appId || process.env.HERE_appId;
-  process.env.HERE_appCode = params.HERE_appCode || process.env.HERE_appCode;
-  process.env.GOOGLE_MAPS_API =
-    params.GOOGLE_MAPS_API || process.env.GOOGLE_MAPS_API;
-  process.env.IBM_FUNCTION_KEY =
-    params.IBM_FUNCTION_KEY || process.env.IBM_FUNCTION_KEY;
-  process.env.REDIS_URL = params.REDIS_URL || process.env.REDIS_URL;
-  process.env.JWT_SECRET = params.JWT_SECRET || process.env.JWT_SECRET;
-  process.env.SENTRY_DNS = params.SENTRY_DNS || process.env.SENTRY_DNS;
+  if (params.POSTGRESS_URI || process.env.POSTGRESS_URI)
+    process.env.POSTGRESS_URI =
+      params.POSTGRESS_URI || process.env.POSTGRESS_URI;
+
+  if (params.MONGO_URI || process.env.MONGO_URI)
+    process.env.MONGO_URI = params.MONGO_URI || process.env.MONGO_URI;
+
+  if (params.MONGO_URI_DEV || process.env.MONGO_URI_DEV)
+    process.env.MONGO_URI_DEV =
+      params.MONGO_URI_DEV || process.env.MONGO_URI_DEV;
+
+  if (params.MONGO_URI_TEST || process.env.MONGO_URI_TEST)
+    process.env.MONGO_URI_TEST =
+      params.MONGO_URI_TEST || process.env.MONGO_URI_TEST;
+
+  if (params.MONGO_URI_LIVE || process.env.MONGO_URI_LIVE)
+    process.env.MONGO_URI_LIVE =
+      params.MONGO_URI_LIVE || process.env.MONGO_URI_LIVE;
+
+  if (params.HERE_appId || process.env.HERE_appId)
+    process.env.HERE_appId = params.HERE_appId || process.env.HERE_appId;
+
+  if (params.HERE_appCode || process.env.HERE_appCode)
+    process.env.HERE_appCode = params.HERE_appCode || process.env.HERE_appCode;
+
+  if (params.GOOGLE_MAPS_API || process.env.GOOGLE_MAPS_API)
+    process.env.GOOGLE_MAPS_API =
+      params.GOOGLE_MAPS_API || process.env.GOOGLE_MAPS_API;
+
+  if (params.IBM_FUNCTION_KEY || process.env.IBM_FUNCTION_KEY)
+    process.env.IBM_FUNCTION_KEY =
+      params.IBM_FUNCTION_KEY || process.env.IBM_FUNCTION_KEY;
+
+  if (params.REDIS_URL || process.env.REDIS_URL)
+    process.env.REDIS_URL = params.REDIS_URL || process.env.REDIS_URL;
+
+  if (params.JWT_SECRET || process.env.JWT_SECRET)
+    process.env.JWT_SECRET = params.JWT_SECRET || process.env.JWT_SECRET;
+
+  if (params.SENTRY_DNS || process.env.SENTRY_DNS)
+    process.env.SENTRY_DNS = params.SENTRY_DNS || process.env.SENTRY_DNS;
 };
