ddc2b8d15e79f937e78652d327ef43840a8d56b4
ParcelSolutions <info@parcelsolutions.eu>
2020-04-10 10:22:39 +0200

    add check al params

--- functions/utils.js
+++ functions/utils.js
@@ -9,3 +9,17 @@ exports.decodeBase64 = body => {
     throw Error("we could not decode the string to an object!");
   }
 };
+
+exports.checkAllParams = object => {
+  // console.log("object values:", Object.values(object));
+  const hasEmptyKey = Object.values(object).some(
+    x => x == null || (typeof x !== "string" && typeof x !== "number")
+  );
+  if (hasEmptyKey) {
+    throw Error(
+      `we should have values for each of the keys:${JSON.stringify(
+        Object.keys(object)
+      )}`
+    );
+  }
+};
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { LogData } = require("./functions/logDNA");
 const { logError } = require("./functions/sentryLogging");
 const closeConnections = require("./functions/closeConnections");
 const { ibmFunctionCall } = require("./functions/ibmFunctionCall");
-const { decodeBase64 } = require("./functions/utils");
+const { decodeBase64, checkAllParams } = require("./functions/utils");
 
 exports.createdDT = createdDT;
 exports.MongoConnection = MongoConnection;
@@ -20,3 +20,4 @@ exports.LogData = LogData;
 exports.closeConnections = closeConnections;
 exports.ibmFunctionCall = ibmFunctionCall;
 exports.decodeBase64 = decodeBase64;
+exports.checkAllParams = checkAllParams;
