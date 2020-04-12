c6f5594de4fe37b8e9a67fd62150a6f269dcbb87
ParcelSolutions <info@parcelsolutions.eu>
2020-04-12 16:39:16 +0200

    remvove duplicate

--- .circleci/pushAction.sh
+++
@@ -1,9 +0,0 @@
-echo "https://$githubUsername:$githubToken@github.com/ParcelSolutions/actionsTransmate.git"
-git clone https://$githubUsername:$githubToken@github.com/ParcelSolutions/actionsTransmate.git
-cd actionsTransmate
-git remote set-url origin https://$githubUsername:$githubToken@github.com/ParcelSolutions/actionsTransmate.git
-git pull
-touch "readme.md"
-git add .
-git commit -m "test"
-git push
