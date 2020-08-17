ed175bcdfc7d2430702f6b0c3455a355b60e1413
ParcelSolutions <info@parcelsolutions.eu>
2020-08-17 23:04:37 +0200

    fix error when logging error

--- functions/sentryLogging.js
+++ functions/sentryLogging.js
@@ -3,29 +3,33 @@ const debug = require("debug")("sentryLogging");
 const { LogData } = require("./logDNA");
 
 function logError(e, type, request) {
-  LogData(e.message, { type, request }, "error");
-  if (
-    process.env.NODE_ENV !== "development" &&
-    process.env.SENTRY_DNS &&
-    typeof process.env.SENTRY_DNS === "string"
-  ) {
-    Sentry.init({
-      release: "serverlessFunction",
-      dsn: process.env.SENTRY_DNS
-    });
+  try {
+    LogData((e || {}).message, { type, request }, "error");
+    if (
+      process.env.NODE_ENV !== "development" &&
+      process.env.SENTRY_DNS &&
+      typeof process.env.SENTRY_DNS === "string"
+    ) {
+      Sentry.init({
+        release: "serverlessFunction",
+        dsn: process.env.SENTRY_DNS
+      });
 
-    Sentry.configureScope(scope => {
-      scope.setExtra("request", request);
-    });
+      Sentry.configureScope(scope => {
+        scope.setExtra("request", request);
+      });
 
-    Sentry.addBreadcrumb({
-      category: type,
-      level: Sentry.Severity.Info
-    });
-    Sentry.captureException(e);
-  } else {
-    console.log("sentry not enabled, set SENTRY_DNS to enable");
-    debug(e, type, request);
+      Sentry.addBreadcrumb({
+        category: type,
+        level: Sentry.Severity.Info
+      });
+      Sentry.captureException(e);
+    } else {
+      console.log("sentry not enabled, set SENTRY_DNS to enable");
+      debug(e, type, request);
+    }
+  } catch (error) {
+    console.error("error while loggin error...", error);
   }
   return null;
 }
