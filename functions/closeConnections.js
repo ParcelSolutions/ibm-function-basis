20fc80a3e34d4fec22af1017062d0ac64e483ecf
Jan Carpentier <jan@transamte.eu>
2021-06-16 11:24:43 +0200

    fix close connections

--- functions/closeConnections.js
+++ functions/closeConnections.js
@@ -2,12 +2,12 @@ const { MongoConnection } = require("../database/mongo");
 const PostgresConnection = require("../database/postgress");
 const RedisConnection = require("../database/redis");
 
-module.exports = async function closeConnections(mongoUri) {
+module.exports = async function closeConnections(mongoUri, pgUri) {
   const mongo = new MongoConnection(mongoUri);
-
+  const pgConnect = new PostgresConnection(pgUri);
   await Promise.all([
     mongo.close(mongoUri),
-    PostgresConnection().close(),
+    pgConnect.close(),
     RedisConnection.close()
   ]);
   console.log("all db connections closed!");
